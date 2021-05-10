067ad0ebb9187540059da2aa15b4538734473cbc
mosle <yskysd@gmail.com>
2021-05-10 13:38:09 +0900

    Add autoSize

--- lib/Dimension/Size.d.ts
+++ lib/Dimension/Size.d.ts
@@ -1,7 +1,9 @@
+import Position from "./Position";
 export default class Size {
     width: number;
     height: number;
     constructor(width: number, height: number);
     mult(scale: number): Size;
     static sizeOfHTMLElement(element: HTMLElement): Size;
+    autoSize(innerSize: Size, cover?: boolean): [number, Position];
 }
--- src/Dimension/Size.ts
+++ src/Dimension/Size.ts
@@ -1,4 +1,5 @@
 import Util from "petit-util";
+import Position from "./Position";
 export default class Size {
   width: number = 0;
   height: number = 0;
@@ -14,5 +15,29 @@ export default class Size {
       Util.dom.computedWidth(element),
       Util.dom.computedHeight(element)
     );
+  }
+  autoSize(innerSize: Size, cover = false): [number, Position] {
+    const frameRatio = this.width / this.height;
+    const innerRatio = innerSize.width / innerSize.height;
+    const position = new Position(0, 0);
+    let scale: number = 1;
+    if (cover) {
+      if (frameRatio > innerRatio) {
+        scale = this.width / innerSize.width;
+        position.y = -(innerSize.height * scale - this.height) / 2;
+      } else {
+        scale = this.height / innerSize.height;
+        position.x = -(innerSize.width * scale - this.width) / 2;
+      }
+    } else {
+      if (frameRatio > innerRatio) {
+        scale = this.height / innerSize.height;
+        position.x = -(innerSize.width * scale - this.width) / 2;
+      } else {
+        scale = this.width / innerSize.width;
+        position.y = -(innerSize.height * scale - this.height) / 2;
+      }
+    }
+    return [scale, position];
   }
 }
