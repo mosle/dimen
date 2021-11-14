ac4df0ded4171748c723784f29e6e2c520b67d69
mosle <yskysd@gmail.com>
2021-11-14 10:26:31 +0900

    build

---
+++ lib/Dimension/Margin.d.ts
@@ -0,0 +1,7 @@
+export default class Margin {
+    top: number;
+    right: number;
+    bottom: number;
+    left: number;
+    constructor(top?: number, right?: number, bottom?: number, left?: number);
+}
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,4 +4,5 @@ import Rect from "./Dimension/Rect";
 import Grid from "./Dimension/Grid";
 import Polygon from "./Dimension/Polygon";
 import Path from "./Dimension/Path";
-export { Size, Position, Rect, Grid, Polygon, Path };
+import Margin from "./Dimension/Margin";
+export { Size, Position, Rect, Grid, Polygon, Path, Margin };
