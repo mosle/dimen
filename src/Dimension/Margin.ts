f8db3e48e7307c5090ef6f000f4fda637dc633be
mosle <yskysd@gmail.com>
2021-11-14 09:29:56 +0900

    add margin class

---
+++ src/Dimension/Margin.ts
@@ -0,0 +1,12 @@
+export default class Margin{
+    top = 0;
+    right = 0;
+    bottom = 0;
+    left = 0;
+    constructor(top:number = 0,right:number = 0,bottom:number = 0,left:number = 0){
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+        this.left = left;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ import Rect from "./Dimension/Rect";
 import Grid from "./Dimension/Grid";
 import Polygon from "./Dimension/Polygon";
 import Path from "./Dimension/Path";
+import Margin from "./Dimension/Margin";
 
-export { Size, Position, Rect, Grid, Polygon, Path };
+export { Size, Position, Rect, Grid, Polygon, Path, Margin };
